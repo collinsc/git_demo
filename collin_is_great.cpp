648c2dcfabf5236fabb79dc08da7f9bc4a26626f
collinsc <collinsconway@gmail.com>
2017-09-30 19:17:13 -0700

    Update collin_is_great.cpp
    You need to incorporate these changes collin

--- collin_is_great.cpp
+++ collin_is_great.cpp
@@ -1,5 +1,7 @@
 #include<iostream> 
 int main(int argc, char** argv) { 
+  //this looks good collin, but you forgot comments
   std::cout << "Collin is really really great!" << std::endl; 
+  //maybe turn that string into a variable?
   return 0; 
 }
